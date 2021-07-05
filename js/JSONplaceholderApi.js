4e7b56dfe8ed6d9cea2d426d323473f5b788c816
AlSivak <olexiysyvak@gmail.com>
2021-07-05 18:52:07 +0300

    todo list done

---
+++ js/JSONplaceholderApi.js
@@ -0,0 +1,66 @@
+class JSONplaceholderApi {
+	constructor() {
+		this.BASE_TODOS_URL = 'https://jsonplaceholder.typicode.com/todos';
+		this.TEST_USER_ID = 'userId=1';
+	}
+
+	getData() {
+		return fetch(`${this.BASE_TODOS_URL}?${this.TEST_USER_ID}`)
+			.then((response) => response.json())
+			.catch((error) => {
+				console.error(error);
+			});
+	}
+
+	saveNewData(userInput) {
+		const options = {
+			method: 'POST',
+			body: JSON.stringify({
+				id: '',
+				title: userInput,
+				completed: false,
+			}),
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8',
+			},
+		};
+
+		return fetch(this.BASE_TODOS_URL, options)
+			.then((response) => response.json())
+			.then(alert('Adding sacceded'))
+			.catch((error) => {
+				console.error(error);
+			});
+	}
+
+	deleteData(targetId) {
+		fetch(`${this.BASE_TODOS_URL}/${targetId}`, {
+			method: 'DELETE',
+		})
+			.then(alert('Deletion sacceded'))
+			.catch((error) => {
+				console.error(error);
+			});
+	}
+
+	changeData(todoToChange) {
+		const options = {
+			method: 'PUT',
+			body: JSON.stringify(todoToChange),
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8',
+			},
+		};
+
+		fetch(`${this.BASE_TODOS_URL}/${todoToChange.id}`, options)
+			.then((response) => response.json())
+			.then(alert('Changing sacceded'))
+			.catch((error) => {
+				console.error(error);
+			});
+	}
+}
+
+const placeholderApi = new JSONplaceholderApi();
+
+export default placeholderApi;
---
+++ js/main.js
@@ -0,0 +1,138 @@
+import placeholderApi from './JSONplaceholderApi.js';
+
+const $formContainer = $('#formContainer');
+const $formInput = $('#formInput');
+const $todosContainer = $('#todosContainer');
+
+let todosList = [];
+let todoToChange;
+
+initTodoList();
+
+async function initTodoList() {
+	initializeFormListeners();
+
+	await placeholderApi.getData().then((todos) => (todosList = todos));
+
+	renderList(todosList);
+
+	initializeListListeners();
+}
+
+function initializeFormListeners() {
+	const FORM_CHANGE_BUTTON_ID = 'formChangeButton';
+
+	$formContainer
+		.on('submit', onFormContainerSubmit)
+		.on('click', '#' + FORM_CHANGE_BUTTON_ID, saveTodoChanges);
+}
+
+function initializeListListeners() {
+	const DELETE_BUTTON_ID = 'deleteButton';
+	const CHANGE_BUTTON_ID = 'changeButton';
+	const TODO_CONTENT_CLASS = 'todo__content';
+
+	$todosContainer
+		.on('click', '#' + DELETE_BUTTON_ID, deleteTodo)
+		.on('click', '#' + CHANGE_BUTTON_ID, changeTodo)
+		.on('click', '.' + TODO_CONTENT_CLASS, changeTodoState);
+}
+
+function onFormContainerSubmit(e) {
+	e.preventDefault();
+
+	const userInput = $formInput.val();
+
+	if (userInput.trim()) {
+		placeholderApi.saveNewData(userInput).then((todo) => addTodo(todo));
+	}
+}
+
+function renderList(todos) {
+	todos.forEach((todo) => renderTodo(todo));
+}
+
+function renderTodo(todo) {
+	const todoTemplate = `<div class="todo container-fluid" data-id=${todo.id}>
+                            <div class="todo__content col-10 ${
+								todo.completed ? 'done' : ''
+							}"><p>${todo.title}</p></div>
+                            <div class="todo__controls col-2">
+                            <button class="btn btn-outline-primary col-5 todo__button" id="changeButton">Change</button><button class="btn btn-outline-danger col-5" id="deleteButton">Delete</button>
+                            </div>
+                        </div>`;
+
+	$todosContainer.prepend(todoTemplate);
+}
+
+function addTodo(todo) {
+	todosList.push(todo);
+
+	renderTodo(todo);
+
+	resetForm();
+}
+
+function deleteTodo(e) {
+	const $targetTodo = $(this).closest('.todo');
+
+	$targetTodo.remove();
+
+	const targetId = $targetTodo.data('id');
+
+	todosList = todosList.filter((todo) => todo.id !== +targetId);
+
+	placeholderApi.deleteData(targetId);
+}
+
+function changeTodoState(e) {
+	const $target = $(this);
+
+	$target.toggleClass('done');
+
+	const targetId = $target.parent().data('id');
+
+	const todoWithChanges = todosList.find((todo) => {
+		if (todo.id === +targetId) {
+			todo.completed = !todo.completed;
+
+			return todo;
+		}
+	});
+
+	placeholderApi.changeData(todoWithChanges);
+}
+
+function changeTodo(e) {
+	const $targetTodo = $(this).closest('.todo');
+
+	const contentToChange = $targetTodo.children('.todo__content').text();
+
+	$formInput.val(contentToChange);
+
+	todoToChange = $targetTodo;
+}
+
+function saveTodoChanges() {
+	const todoToChangeId = todoToChange.data('id');
+
+	const newTodoContent = $formInput.val();
+
+	todoToChange.children('.todo__content').html(`<p>${newTodoContent}</p>`);
+
+	resetForm();
+
+	const todoWithChanges = todosList.find((todo) => {
+		if (todo.id === +todoToChangeId) {
+			todo.title = newTodoContent;
+
+			return todo;
+		}
+	});
+
+	placeholderApi.changeData(todoWithChanges);
+}
+
+function resetForm() {
+	$formInput.val('');
+}
